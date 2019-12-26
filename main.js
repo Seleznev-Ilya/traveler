3cdbab1f78ea8eb80f44905f714fdbe676f4a893
Ilia Selieznov <ziziron21@gmail.com>
2019-12-26 17:15:59 +0200

    first page

---
+++ main.js
@@ -0,0 +1,29 @@
+
+// function fullScreen(element) {
+//     if (element.requestFullscreen) {
+//         element.requestFullscreen();
+//     } else if (element.webkitrequestFullscreen) {
+//         element.webkitRequestFullscreen();
+//     } else if (element.mozRequestFullscreen) {
+//         element.mozRequestFullScreen();
+//     }
+// }
+// // var html = document.documentElement;
+//
+// fullScreen(html);
+
+// var offset = 200;
+// var menu = document.getElementById('main-menu');
+document.getElementById('button').onclick = document.getElementById('main-menu').style.right = -200 + 'px';
+
+
+// function right() {
+// if (offset === 200) {
+//     offset = 0;
+// menu.style.right = offset + 'px';
+// } else {
+//     offset = 200;
+//     menu.style.right = offset + 'px';
+// }
+
+}
